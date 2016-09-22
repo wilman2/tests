7d8e3de34057089a950084ee2ffcf0b270d383c9
wilman <rafal.wilmanowicz@gmail.com>
2016-09-22 09:31:15 +0200

    testdouble library

--- test/file/fetchSymbolsTest.js
+++ test/file/fetchSymbolsTest.js
@@ -1,10 +1,28 @@
 const assert = require('assert');
 const fetchSymbols = require('../../lib/file/fetchSymbols');
 const coMocha = require('co-mocha');
+const td = require('testdouble');
 
 describe('fetch symbols', function () {
-    it('reads file and parses symbols', function *() {
+    it('[mocking library] reads file and parses symbols', function *() {
+        //given
+        const readFile = td.function('readFile');
+        td.when(readFile('someFile')).thenReturn(Promise.resolve('file content'));
+        const parseSymbols = td.function('parseSymbols');
+        td.when(parseSymbols('file content')).thenReturn(['A','B']);
 
+        const fetch = fetchSymbols({readFile, parseSymbols});
+
+        //when
+        const symbols = yield fetch('someFile');
+
+        //then
+        td.verify(readFile('someFile'));
+        td.verify(parseSymbols('file content'));
+        assert.deepEqual(symbols, ['A', 'B']);
+    });
+
+    it('reads file and parses symbols', function *() {
         //given
         var expectedAssertionCount = 0;
 
